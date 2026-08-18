9063feeb152b2e2c9e47183e951479eced1b7494
alekseishporta-dev <alekseishporta@gmail.com>
2026-08-18 16:36:56 +0000

    refgr

---
+++ infrastructure-modules/k3-vault/main.tf
@@ -0,0 +1,60 @@
+terraform {
+    required_providers {
+        helm = {
+            source = "hashicorp/helm"
+            version = "~> 2.12.0"
+        }
+    }
+}
+
+provider "helm" {
+    kubernetes {
+        config_path = "~/.kube/config"
+    }
+}
+
+variable "env_name" {
+  type        = string
+  description = "Имя окружения (dev или prod)"
+}
+
+resource "helm_release" "vault" {
+  name             = "${var.env_name}-vault"
+  repository       = "https://hashicorp.com"
+  chart            = "vault"
+  namespace        = var.env_name
+  create_namespace = true
+
+  # Настройки Vault передаем через блок values
+  values = [
+    <<-EOF
+    server:
+      # Включаем режим standalone (для одной ноды K3s)
+      standalone:
+        enabled: true
+      
+      # Ограничиваем ресурсы, чтобы не перегрузить виртуалку
+      resources:
+        limits:
+          cpu: 200m
+          memory: 256Mi
+        requests:
+          cpu: 100m
+          memory: 128Mi
+
+      # Открываем веб-интерфейс Vault наружу через NodePort
+      service:
+        type: NodePort
+        nodePort: ${var.env_name == "dev" ? 31000 : 31001}
+
+    # Отключаем встроенный инжектор секретов (пока он нам не нужен для простоты)
+    injector:
+      enabled: false
+
+    # Включаем удобный графический веб-интерфейс Vault (UI)
+    ui:
+      enabled: true
+      serviceType: "NodePort"
+    EOF
+  ]
+}
